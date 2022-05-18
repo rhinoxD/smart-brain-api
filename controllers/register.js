cff6b9bef17827cb4be0df7820b7a892a1ab971e
unknown <coolshiv0721@gmail.com>
2022-05-18 10:15:14 +0530

    Moved register code from server to register controller

---
+++ controllers/register.js
@@ -0,0 +1,29 @@
+const handleRegister = (req, res, db, bcrypt, saltRounds) => {
+  const { name, email, password } = req.body;
+  const hash = bcrypt.hashSync(password, saltRounds);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.status(201).json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('Unable to Register.'));
+};
+
+module.exports = { handleRegister };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const someOtherPlaintextPassword = 'not_bacon';
 const knex = require('knex');
+const register = require('./controllers/register')
 
 const db = knex({
   client: 'pg',
@@ -94,31 +95,7 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password, saltRounds);
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into('login')
-      .returning('email')
-      .then((loginEmail) => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.status(201).json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json('Unable to Register.'));
+  register.handleRegister(req, res, db, bcrypt, saltRounds);
 });
 
 app.put('/image', (req, res) => {
