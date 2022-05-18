c3d9441302bae2d2a69cbbd84d38c2d3ab52ce0e
unknown <coolshiv0721@gmail.com>
2022-05-18 10:28:27 +0530

    Moved image code from server to image controller

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -20,8 +21,8 @@ const db = knex({
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -56,26 +57,14 @@ const database = {
 app.get('/', (req, res) => {
   res.send(database.users);
 });
-
 app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
-
 app.post('/register', (req, res) => {
   register.handleRegister(req, res, db, bcrypt, saltRounds);
 });
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then((entries) => {
-      res.json(entries[0]);
-    })
-    .catch((err) => res.status(400).json('Unable to get entries'));
-});
+app.put('/image', (req, res) => image.handleImage(req, res, db));
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}...`);
